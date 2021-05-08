d70edaaffcb8333789343597189a94400f648f08
Victor <publicdomainvic@gmail.com>
2021-05-08 00:14:12 -0400

    Add OnAboutToolStripMenuItemClick() code

--- Enfolder/MainForm.cs
+++ Enfolder/MainForm.cs
@@ -11,14 +11,22 @@ namespace Enfolder
     using System.Diagnostics;
     using System.Drawing;
     using System.IO;
+    using System.Reflection;
     using System.Windows.Forms;
     using Microsoft.Win32;
+    using PublicDomain;
 
     /// <summary>
     /// Description of MainForm.
     /// </summary>
     public partial class MainForm : Form
     {
+        /// <summary>
+        /// Gets or sets the associated icon.
+        /// </summary>
+        /// <value>The associated icon.</value>
+        private Icon associatedIcon = null;
+
         /// <summary>
         /// The enfolder key list.
         /// </summary>
@@ -32,11 +40,19 @@ namespace Enfolder
             // The InitializeComponent() call is required for Windows Forms designer support.
             this.InitializeComponent();
 
+            /* Set icons */
+
+            // Set associated icon from exe file
+            this.associatedIcon = Icon.ExtractAssociatedIcon(typeof(MainForm).GetTypeInfo().Assembly.Location);
+
+            // Set public domain weekly tool strip menu item image
+            this.moreReleasesPublicDomainGiftcomToolStripMenuItem.Image = this.associatedIcon.ToBitmap();
+
             // Update GUI
             this.UpdateByRegistryKey();
         }
 
-        /// <summary>
+        /// <summary>https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwi7ofuijLnwAhVnGVkFHVNtBBcQFjABegQIAhAD&url=http%3A%2F%2Fsacredscribesangelnumbers.blogspot.com%2F2015%2F09%2Fangel-number-2301.html%23%3A~%3Atext%3DNumber%25202301%2520is%2520a%2520blend%2Cthe%2520attributes%2520of%2520number%25201.%26text%3DAngel%2520Number%25202301%2520encourages%2520you%2Cand%2520into%2520that%2520of%2520others.&usg=AOvVaw3KKED-tKtiGyaRoy0yTCBe
         /// Handles the add button click event.
         /// </summary>
         /// <param name="sender">Sender object.</param>
@@ -185,7 +201,52 @@ namespace Enfolder
         /// <param name="e">Event arguments.</param>
         private void OnAboutToolStripMenuItemClick(object sender, EventArgs e)
         {
-            // TODO Add code
+            // Set license text
+            var licenseText = $"CC0 1.0 Universal (CC0 1.0) - Public Domain Dedication{Environment.NewLine}" +
+                $"https://creativecommons.org/publicdomain/zero/1.0/legalcode{Environment.NewLine}{Environment.NewLine}" +
+                $"Libraries and icons have separate licenses.{Environment.NewLine}{Environment.NewLine}" +
+                $"Documents icon by OpenClipart-Vectors - Pixabay License{Environment.NewLine}" +
+                $"https://pixabay.com/vectors/documents-files-folders-paper-159116/{Environment.NewLine}{Environment.NewLine}" +
+                $"Patreon icon used according to published brand guidelines{Environment.NewLine}" +
+                $"https://www.patreon.com/brand{Environment.NewLine}{Environment.NewLine}" +
+                $"GitHub mark icon used according to published logos and usage guidelines{Environment.NewLine}" +
+                $"https://github.com/logos{Environment.NewLine}{Environment.NewLine}" +
+                $"DonationCoder icon used with permission{Environment.NewLine}" +
+                $"https://www.donationcoder.com/forum/index.php?topic=48718{Environment.NewLine}{Environment.NewLine}" +
+                $"PublicDomain icon is based on the following source images:{Environment.NewLine}{Environment.NewLine}" +
+                $"Bitcoin by GDJ - Pixabay License{Environment.NewLine}" +
+                $"https://pixabay.com/vectors/bitcoin-digital-currency-4130319/{Environment.NewLine}{Environment.NewLine}" +
+                $"Letter P by ArtsyBee - Pixabay License{Environment.NewLine}" +
+                $"https://pixabay.com/illustrations/p-glamour-gold-lights-2790632/{Environment.NewLine}{Environment.NewLine}" +
+                $"Letter D by ArtsyBee - Pixabay License{Environment.NewLine}" +
+                $"https://pixabay.com/illustrations/d-glamour-gold-lights-2790573/{Environment.NewLine}{Environment.NewLine}";
+
+            // Prepend sponsors
+            licenseText = $"RELEASE SPONSORS:{Environment.NewLine}{Environment.NewLine}* Jesse Reichler{Environment.NewLine}{Environment.NewLine}=========={Environment.NewLine}{Environment.NewLine}" + licenseText;
+
+            // Set title
+            string programTitle = typeof(MainForm).GetTypeInfo().Assembly.GetCustomAttribute<AssemblyTitleAttribute>().Title;
+
+            // Set version for generating semantic version 
+            Version version = typeof(MainForm).GetTypeInfo().Assembly.GetName().Version;
+
+            // Set about form
+            var aboutForm = new AboutForm(
+                $"About {programTitle}",
+                $"{programTitle} {version.Major}.{version.Minor}.{version.Build}",
+                $"Made for: Jasong222{Environment.NewLine}DonationCoder.com{Environment.NewLine}Day #128, Week #18 @ May 08, 2021",
+                licenseText,
+                this.Icon.ToBitmap())
+            {
+                // Set about form icon
+                Icon = this.associatedIcon,
+
+                // Set always on top
+                TopMost = this.TopMost
+            };
+
+            // Show about form
+            aboutForm.ShowDialog();
         }
     }
 }
